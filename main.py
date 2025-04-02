1dd44148ffbccba82f32262e6de23d1556ac16d8
Dtrox4 <88051551+Dtrox4@users.noreply.github.com>
2025-04-02 20:43:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -51,23 +51,19 @@ keep_alive()  # Keep the bot alive
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
-    # Set the presence status
-    await bot.change_presence(activity=discord.Game(name="if you're worthy,  you shall be skulled"))
+    await bot.change_presence(activity=discord.Game(name="if you're worthy, you shall be skulled"))
 
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
 
-    # Check if it's a DM
     if isinstance(message.channel, discord.DMChannel):
         print(f"DM from {message.author}: {message.content}")
 
-    # Skull reactions for authorized users
     if message.author.id in bot.user_skull_list:
-        await message.add_reaction("☠️")  # Skull and crossbones reaction
+        await message.add_reaction("\u2620\ufe0f")  # Skull and crossbones reaction
 
-    # Check if message is a command
     if message.content.startswith("!skull"):
         if message.author.id not in AUTHORIZED_USERS:
             await message.channel.send("You do not have permission to use this command.")
@@ -75,26 +71,16 @@ async def on_message(message):
 
         args = message.content.split()
 
-        # Command to show the list of authorized users
         if len(args) == 2 and args[1] == "authorized":
-            if AUTHORIZED_USERS:
-                authorized_users = [f'<@{user_id}>' for user_id in AUTHORIZED_USERS]
-                await message.channel.send(f"Authorized users: {', '.join(authorized_users)}")
-            else:
-                await message.channel.send("No users are authorized.")
+            authorized_users = [f'<@{user_id}>' for user_id in AUTHORIZED_USERS]
+            await message.channel.send(f"Authorized users: {', '.join(authorized_users)}")
             return
 
-        # Command to show the list of users being skulled
         if len(args) == 2 and args[1] == "list":
-            if bot.user_skull_list:
-                skull_users = [f'<@{user_id}>' for user_id in bot.user_skull_list]
-                await message.channel.send(f"Users being skulled: {', '.join(skull_users)}")
-            else:
-                await message.channel.send("No users are currently being skulled.")
+            skull_users = [f'<@{user_id}>' for user_id in bot.user_skull_list]
+            await message.channel.send(f"Users being skulled: {', '.join(skull_users)}" if skull_users else "No users are currently being skulled.")
             return
 
-        
-        # Command to show the list of available commands in an embed format
         if len(args) == 2 and args[1] == "help":
             embed = discord.Embed(
                 title="Worthy Commands",
@@ -107,26 +93,23 @@ async def on_message(message):
             embed.add_field(name="!skull authorized", value="Show authorized users.", inline=False)
             embed.add_field(name="!skull authorize @user", value="Authorize a user to use commands.", inline=False)
             embed.add_field(name="!skull help", value="Show this help message.", inline=False)
-            embed.set_footer(text="autoskull bot - made by @xv9c")
-            
+            embed.set_footer(text="AutoSkull Bot - Made by @xv9c")
             await message.channel.send(embed=embed)
             return
 
-        # Command to add a user to authorized list
         if len(args) == 3 and args[1].lower() == "authorize":
-            if len(message.mentions) == 1:
+            if message.mentions:
                 user = message.mentions[0]
                 if user.id not in AUTHORIZED_USERS:
                     AUTHORIZED_USERS.add(user.id)
-                    await message.channel.send(f"{user.mention} has been authorized to use the commands.")
+                    await message.channel.send(f"{user.mention} has been authorized.")
                 else:
                     await message.channel.send(f"{user.mention} is already authorized.")
             else:
                 await message.channel.send("Please mention a valid user to authorize.")
 
-        # Command to stop skulling a user
         if len(args) == 3 and args[1].lower() == "stop":
-            if len(message.mentions) == 1:
+            if message.mentions:
                 user = message.mentions[0]
                 if user.id in bot.user_skull_list:
                     bot.user_skull_list.remove(user.id)
@@ -136,15 +119,12 @@ async def on_message(message):
             else:
                 await message.channel.send("Please mention a valid user to stop skulling.")
 
-        # Command to skull a user (add to list of users to skull)
         elif len(args) == 2:
             mentioned_users = message.mentions
             if mentioned_users:
                 for user in mentioned_users:
                     bot.user_skull_list.add(user.id)
-                await message.channel.send(
-                    f"Will skull {', '.join([user.mention for user in mentioned_users])} from now on ☠️"
-                )
+                await message.channel.send(f"Will skull {', '.join([user.mention for user in mentioned_users])} from now on ☠️")
             else:
                 await message.channel.send("Please mention a user to skull!")
 
