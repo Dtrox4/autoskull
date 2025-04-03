bb5872d0270558be15d4437c8b8bd0c653c07710
Dtrox4 <88051551+Dtrox4@users.noreply.github.com>
2025-04-04 00:26:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -175,7 +175,7 @@ async def on_message(message):
         await message.channel.send(embed=embed)
         return
 
-    if len(args) == 3 and args[1].lower() == "authorize" and mentioned_users:
+if len(args) == 3 and args[1].lower() == "authorize" and mentioned_users:
     user = mentioned_users[0]
     if user.id not in AUTHORIZED_USERS:
         AUTHORIZED_USERS.add(user.id)  # Add user to the set
@@ -185,7 +185,7 @@ async def on_message(message):
         await message.channel.send(f"{user.mention} is already authorized.")
     return
 
-    if len(args) == 3 and args[1].lower() == "unauthorize" and mentioned_users:
+if len(args) == 3 and args[1].lower() == "unauthorize" and mentioned_users:
     user = mentioned_users[0]
     if user.id in AUTHORIZED_USERS and user.id != YOUR_USER_ID:
         AUTHORIZED_USERS.remove(user.id)  # Remove from set
