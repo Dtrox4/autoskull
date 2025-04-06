1f830bb12515f551fc5fa8d03ed2c604e9419f4d
Dtrox4 <88051551+Dtrox4@users.noreply.github.com>
2025-04-06 16:14:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -62,17 +62,7 @@ intents.members = True
 
 bot = AutoSkullBot(intents)
 
-@app.route('/')
-def home():
-    return "Bot is running!"
-
-app = Flask(__name__)
-
-if __name__ == '__main__':
-    def run_flask():
-        app.run(host='0.0.0.0', port=3000)
-
-    Thread(target=run_flask).start()
+load_flask()
 
 @bot.command()
 async def skull(ctx, subcommand=None, *args):
