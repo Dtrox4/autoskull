f6e447f9503b1d7764eaa9101210808e8ec0cfa9
Dtrox4 <88051551+Dtrox4@users.noreply.github.com>
2025-04-07 19:35:32 +0530

    Update simple.py

--- simple.py
+++ simple.py
@@ -266,9 +266,6 @@ async def on_message(message):
                 await message.channel.send(embed=embed)
             return
 
-         await.botprocess_commands(message)
-    
-
     if message.author.id in bot.user_skull_list:
         await message.add_reaction("\u2620\ufe0f")
 
