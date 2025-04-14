d2a0d4a9c4e6f84bf7c28620194684e92ee267ff
xv9c <88051551+Dtrox4@users.noreply.github.com>
2025-04-14 11:57:22 +0530

    Update simple.py

--- simple.py
+++ simple.py
@@ -59,7 +59,7 @@ intents.members = True
 intents.dm_messages = True
 
 # initializing the bot
-class AutoSkullBot(discord.Client):
+class AutoSkull(discord.Client):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.user_skull_list = set()
